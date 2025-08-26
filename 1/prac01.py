54e8cd19698aa6579584f654e42b45212f72c053
ProfSunKim <141034435+ProfSunKim@users.noreply.github.com>
2025-08-26 10:51:09 +0900

    Update prac01.py

--- 1/prac01.py
+++ 1/prac01.py
@@ -1,1 +1,19 @@
+import pygame
 
+pygame.init()
+
+# 화면 설정
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("본인 학번, 이름")
+
+# 게임 루프
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
