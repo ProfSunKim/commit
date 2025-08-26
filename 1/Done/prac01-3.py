136dda3e7cef8b696e14c16a3f128a5bf570f912
ProfSunKim <141034435+ProfSunKim@users.noreply.github.com>
2025-08-26 19:32:05 +0900

    Create prac01-3.py

---
+++ 1/Done/prac01-3.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+
+# 화면 설정
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("본인 학번, 이름")
+clock = pygame.time.Clock()
+
+# 캐릭터 설정
+#character = pygame.Rect(50, 50, 50, 50)
+speed = 1
+charcter_image = pygame.image.load('C:/Users/sunje/Desktop/Commit/school.png')
+charcter_position = [50, 50]
+
+# 게임 루프
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        #character.x -= speed
+        charcter_position[0] -= speed
+    if keys[pygame.K_RIGHT]:
+        #character.x += speed
+        charcter_position[0] += speed
+    if keys[pygame.K_UP]:
+        #character.y -= speed
+        charcter_position[1] -= speed
+    if keys[pygame.K_DOWN]:
+        #character.y += speed
+        charcter_position[1] += speed
+    
+    screen.fill((128, 128, 128)) # 배경색
+    #pygame.draw.rect(screen, (0, 128, 255), character) # 캐릭터
+    screen.blit(charcter_image, charcter_position)
+    pygame.display.update()
+
+    clock.tick(60) # 60 FPS
+
+pygame.quit()
