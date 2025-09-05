dce8a155c8fc1151d6bf907c5c9b438d84b4c191
ProfSunKim <141034435+ProfSunKim@users.noreply.github.com>
2025-09-05 20:24:28 +0900

    Update prac03-9.py

--- prac03/Done/prac03-9.py
+++ prac03/Done/prac03-9.py
@@ -59,14 +59,14 @@ class Creature:
         self.hp = hp            # 현재 체력
         self.attack = attack    # 공격력
         self.skills = skills    # 가지고 있는 기술 리스트
-    
+
+     # 데미지를 입음
+    def take(self, damage):
+        self.hp = max(0, self.hp - damage)  # 체력은 0 미만으로 내려가지 않음
+
     # 살아있는지 확인
     def alive(self):
         return self.hp > 0
-
-    # 데미지를 입음
-    def take(self, damage):
-        self.hp = max(0, self.hp - damage)  # 체력은 0 미만으로 내려가지 않음
 
 # --------------------------
 # 공격 처리 함수
@@ -85,84 +85,92 @@ def attack(attacker, target, skill):
 # 게임 실행
 # --------------------------
 # 플레이어 캐릭터 생성
-player = Creature("Hero", 50, 5, [Skill("Slash", 4, 95), Skill("Fire", 6, 85)])
+player = Creature("Hero", 50, 5, [Skill("Slash", 4, 85), Skill("Fire", 6, 75)])
 
 # 적 캐릭터 생성
 enemies = [
-    Creature("SlimeA", 30, 3, [Skill("Bite", 3, 90)]),
-    Creature("SlimeB", 25, 3, [Skill("Bite", 3, 90)]),
-    Creature("SlimeC", 20, 2, [Skill("Bite", 2, 95)])
+    Creature("SlimeA", 30, 3, [Skill("Bite", 3, 80)]),
+    Creature("SlimeB", 25, 3, [Skill("Bite", 3, 85)]),
+    Creature("SlimeC", 20, 2, [Skill("Bite", 2, 90)])
 ]
 
 # --------------------------
 # 게임 루프
 # --------------------------
-options = ["Fight","Run"]
-choice, menu = -1, 0
+options = ["Fight","Run"]                           # 메뉴 버튼 글자
+choice, level = -1, 0                               # 선택한 메뉴 (choice=0 첫번째 메뉴, choice=2 두번째 메뉴, ...), 메뉴 레벨
 message = "Enemies have appeared!"
+btn_width, btn_height, btn_gap = 60, 30, 40         # 메뉴 버튼 크기, 간격 (변경 가능)
 running, gameover = True, False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:               # 종료 버튼 누르면 게임 종료
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:    # 마우스 버튼이 눌렸을 때
-            choice = -1
             mouse_pos = pygame.mouse.get_pos()      # 마우스의 포지션을 받아서 mouse_pos에 저장
-            button_width, button_height = 60, 30    # 메뉴 버튼 크기 (변경 가능)
             # ----------------------
             # 메인 메뉴 선택 (Fight / Run)
             # ----------------------
-            if menu == 0:
+            if level == 0:
                 for i, option in enumerate(options):
-                    button = pygame.Rect(screen_width//2-button_width//2, 400+i*40-button_height//2, button_width, button_height)
+                    button = pygame.Rect(screen_width//2-btn_width//2, 400+i*btn_gap-btn_height//2, btn_width, btn_height)
                     if mouse_pos[0] > button.x and mouse_pos[0] < button.x+button.width:
                         if mouse_pos[1] > button.y and mouse_pos[1] < button.y+button.height:
                             choice = i
-                if choice == 0:                     # Fight 선택
-                    menu, choice = 1, -1
-                else:                               # Run 선택
+                if choice == 0:                     # Fight 메뉴 선택
+                    level, choice = 1, -1
+                elif choice == 1:                   # Run 메뉴 선택
                     message = "You can't run away!"
+                
             # ----------------------
             # 스킬 선택
             # ----------------------
-            elif menu == 1:
+            elif level == 1:
                 for i, skill in enumerate(player.skills):
-                    button = pygame.Rect(screen_width//2-button_width//2, 400+i*40-button_height//2, button_width, button_height)
+                    button = pygame.Rect(screen_width//2-btn_width//2, 400+i*btn_gap-btn_height//2, btn_width, btn_height)
                     if mouse_pos[0] > button.x and mouse_pos[0] < button.x+button.width:
                         if mouse_pos[1] > button.y and mouse_pos[1] < button.y+button.height:
                             choice = i
-                back_button = pygame.Rect(screen_width//2-button_width//2, 400+(i+1)*40-button_height//2, button_width, button_height)
+                back_button = pygame.Rect(screen_width//2-btn_width//2, 400+(i+1)*btn_gap-btn_height//2, btn_width, btn_height)
                 if mouse_pos[0] > back_button.x and mouse_pos[0] < back_button.x+back_button.width:
                     if mouse_pos[1] > back_button.y and mouse_pos[1] < back_button.y+back_button.height:
                         choice = i+1
+                
+                if choice >= 0:                     # Skill 선택 후 Attack 버튼 선택 가능
+                    attack_button = pygame.Rect(screen_width//2 + btn_width//2, 400+(i+1)*btn_gap-btn_height//2, btn_width, btn_height)
+                    if mouse_pos[0] > attack_button.x and mouse_pos[0] < attack_button.x+attack_button.width:
+                        if mouse_pos[1] > attack_button.y and mouse_pos[1] < attack_button.y+attack_button.height:
+                            message = "You choose " + player.skills[choice].name + "\n Click one Slime!"
+                            level = 2
+                
                 if choice == i+1:                   # Back 버튼 선택
-                    menu, choice = 0, -1
-                else:                               # Skill 선택
-                    message = "You choose " + player.skills[choice].name
-                    menu = 2
+                    level, choice = 0, -1
+                #elif choice >= 0:                   # Skill 선택
+                #    message = "You choose " + player.skills[choice].name
+                #elif choice == -1:
+                #    message = "Please select one"
+
             # ----------------------
             # 공격 대상 선택
             # ----------------------
-            elif menu == 2:
+            elif level == 2:
                 gap, left = 150, (screen_width - (len(enemies)-1) * 150) // 2
                 for i, enemy in enumerate(enemies):
                     x, y = left + i*gap, 120
                     if mouse_pos[0] > x and mouse_pos[0] < x+80 and mouse_pos[1] > y and mouse_pos[1] < y+80:
                         target = enemies[i]
-                        log = [attack(player, target, player.skills[choice])]   # 플레이어 공격
+                        log = [attack(player, target, player.skills[choice])]               # 플레이어가 적을 공격
                         for enemy in enemies:
-                            if enemy.alive():                                   # 살아있는 적이 공격
-                                log.append(attack(enemy, player, random.choice(enemy.skills)))
-                                if not player.alive(): break
+                            log.append(attack(enemy, player, random.choice(enemy.skills)))  # 적이 플레이어를 공격
                         message = "\n".join(log)
+                        level, choice = 0, -1
                         # 승리/패배 체크
-                        if not any(enemy.alive() for enemy in enemies):
-                            message += "\nVictory!"
-                            gameover = True
                         if not player.alive():
-                            message += "\nDefeat..."
+                            message += "\nDefeat...T_T"
                             gameover = True
-                        menu = 0
+                        elif not any(enemy.alive() for enemy in enemies):
+                            message += "\nVictory!!!"
+                            gameover = True
 
     # ----------------------
     # 화면 그리기
@@ -173,13 +181,12 @@ while running:
     gap, left = 150, (screen_width - (len(enemies)-1) * 150) // 2
     for i, enemy in enumerate(enemies):
         x, y = left + i*gap, 120
-        #pygame.draw.rect(screen, colors["RED"], (x, y, 80, 80))
         pygame.draw.rect(screen, colors["RED"] if enemy.alive() else colors["WHITE"], (x, y, 80, 80))
         draw_text(screen,font, enemy.name, (x+40, y+90), colors["WHITE"], True)
         draw_hp(screen , x, y-20, 80, 10, enemy.hp, enemy.max, colors)
 
     # 플레이어 그리기
-    pygame.draw.rect(screen, colors["BLUE"], (100,300,80,80))
+    pygame.draw.rect(screen, colors["BLUE"] if player.alive() else (100,100,100), (100,300,80,80))
     draw_text(screen, font, player.name, (140,390), colors["WHITE"], True)
     draw_hp(screen, 80, 280, 120, 12, player.hp, player.max, colors)
 
@@ -189,13 +196,15 @@ while running:
 
     # 메뉴 옵션 표시
     if not gameover:
-        if menu == 0:
+        if level == 0:
             for i, option in enumerate(options):
-                draw_text(screen, font, option, (screen_width//2, 400+i*40), (0,200,200) if i==choice else colors["WHITE"], True)
-        elif menu == 1 or menu == 2:
+                draw_text(screen, font, option, (screen_width//2, 400+i*btn_gap), (0,200,200) if i==choice else colors["WHITE"], True)
+        elif level == 1:
             for i, skill in enumerate(player.skills):
-                draw_text(screen, font, skill.name, (screen_width//2, 400+i*40), (0,200,200) if i==choice else colors["WHITE"], True)
-            draw_text(screen, font, "Back", (screen_width//2, 400+(i+1)*40), colors["WHITE"], True)
+                draw_text(screen, font, skill.name, (screen_width//2, 400+i*btn_gap), (0,200,200) if i==choice else colors["WHITE"], True)
+            draw_text(screen, font, "Back", (screen_width//2, 400+(i+1)*btn_gap), colors["WHITE"], True)
+            if choice >= 0:
+                draw_text(screen, font, "Attack", (screen_width//2 + btn_width, 400+(i+1)*btn_gap), (200, 0, 200), True)
 
     pygame.display.flip()                   # 화면 업데이트
     clock.tick(60)                          # 초당 60프레임
